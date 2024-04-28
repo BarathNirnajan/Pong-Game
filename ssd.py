2a70d4dc859a8d41b3b70471b9ec819971db01ec
BarathNiranjanSA <d21l602@psgitech.ac.in>
2024-04-28 15:16:38 +0530

    upload files

---
+++ ssd.py
@@ -0,0 +1,13 @@
+
+# for _ in range(0, no_of_lines):
+#     list_of_words = []
+#     words = str(input())
+#
+#     for word in words:
+#         list_of_words.append(word)
+#     length = len(list_of_words)
+#
+#     if length > 10:
+#         print(f"{list_of_words[0]}{length - 2}{list_of_words[length-1]}")
+#     else:
+#         print(words)
